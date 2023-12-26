4f3c6977161181e8aa089ffc1b75737bfe48564a
MaybleJul <mayblejul@gmail.com>
2023-12-26 22:56:45 +0400

    feat: basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import  styled  from "styled-components"
 import Hero from "./components/Hero"
 import About from "./components/About"
 import Works from "./components/Works"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
-import Navbar from './Navbar'
 
 const Section = styled.div`
     height: 100vh;
     scroll-snap-align: center;
   `
 
-const Hero = () =>{
+const Navbar = () =>{
     return (
-        <Section>
-           <Navbar/> 
-        </Section>
+        <Section>Navbar</Section>
     )
 }
 
-export default Hero
+export default Navbar
